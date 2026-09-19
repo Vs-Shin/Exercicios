5fc503b435d94c33e6160947ddee145b139bf108
Vs-Shin <Visualsymbols@gmail.com>
2026-09-18 22:19:21 -0300

    Outro exercício.

--- ex024.py
+++ ex024.py
@@ -1,6 +1,2 @@
 nome = input("Digite o nome de uma cidade. ").strip().split()[0].title()
-
-if nome == "Santo":
-    print("O nome da cidade começa com Santo.")
-else:
-    print("O nome da cidade não começa com Santo.")
+print(nome[0:5] == "Santo")
